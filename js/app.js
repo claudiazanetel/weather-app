f7443fdcb04e3b49f37a9be94a349deb2642691b
claudia.zanetel <claudia.zanetel@gmail.com>
2017-04-27 23:55:12 +0200

    choose the coordinates (lat e lon) and get return the weather data

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ angular.module('weatherApp', ['ngResource'])
         	return $resource('http://worksample-api.herokuapp.com/weather?q=:city&key=62fc4256-8f8c-11e5-8994-feff819cdc9f', {city:'@city'});
     	};
 
+    	this.getWeatherByCoord = function() {
+    		return $resource('http://worksample-api.herokuapp.com/weather?lat=:lat&lon=:lon&key=62fc4256-8f8c-11e5-8994-feff819cdc9f', {lat:'@lat', lon:'@lon'});
+    	};
+
 	}])
 
 	.controller('MenuController', ['$scope', 'fetchWeatherData', function ($scope,fetchWeatherData ) {
@@ -19,7 +23,16 @@ angular.module('weatherApp', ['ngResource'])
 
 		};
 
+		$scope.lat=''
+		$scope.lon=''
+		$scope.chooseCoord = function() {
 
+		fetchWeatherData.getWeatherByCoord().get({lat:$scope.lat, lon:$scope.lon})
+	    	.$promise.then(function(data) {
+	      		console.log(data);
+	    	});
+
+	    };	
 	}])
 
 
