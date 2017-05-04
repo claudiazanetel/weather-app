7fd03ab56560da79b5dc3b12f35684c12416907f
claudia.zanetel <claudia.zanetel@gmail.com>
2017-05-04 21:55:25 +0200

    better error handling

--- js/app.js
+++ js/app.js
@@ -3,22 +3,23 @@ angular.module('weatherApp', ['ngResource'])
 
         this.getWeatherByCity = function() {
         	return $resource('http://worksample-api.herokuapp.com/weather?q=:city&key=62fc4256-8f8c-11e5-8994-feff819cdc9f', {city:'@city'});
-    		//return $resource('http://api.openweathermap.org/data/2.5/weather?q=:city&appid=4b80da9843e489246022d72d79a1e508', {city:'@city'});
+    		//return $resource('http://api.openweathermap.org/data/2.5/weather?q=:city&units=metric&appid=4b80da9843e489246022d72d79a1e508', {city:'@city'});
 
     	};
 
     	this.getSummaryWeatherByCity = function() {
     		return $resource('http://worksample-api.herokuapp.com/forecast/daily?q=:city&key=62fc4256-8f8c-11e5-8994-feff819cdc9f', {city:'@city'});
-    		//return $resource('http://api.openweathermap.org/data/2.5/forecast/daily?q=:city&appid=4b80da9843e489246022d72d79a1e508', {city:'@city'});
+    		//return $resource('http://api.openweathermap.org/data/2.5/forecast/daily?q=:city&units=metric&appid=4b80da9843e489246022d72d79a1e508', {city:'@city'});
     	};
 
     	this.getDetailedWeatherByCity = function() {
     		return $resource('http://worksample-api.herokuapp.com/forecast?q=:city&key=62fc4256-8f8c-11e5-8994-feff819cdc9f', {city:'@city'});
-    		//return $resource('http://api.openweathermap.org/data/2.5/forecast?q=:city&appid=4b80da9843e489246022d72d79a1e508', {city:'@city'});
+    		//return $resource('http://api.openweathermap.org/data/2.5/forecast?q=:city&units=metric&appid=4b80da9843e489246022d72d79a1e508', {city:'@city'});
     	};
 
 	   	this.getWeatherByCoord = function() {
     		return $resource('http://worksample-api.herokuapp.com/weather?lat=:lat&lon=:lon&key=62fc4256-8f8c-11e5-8994-feff819cdc9f', {lat:'@lat', lon:'@lon'});
+    		//return $resource('http://api.openweathermap.org/data/2.5/weather?lat=:lat&lon=:lon&units=metric&appid=4b80da9843e489246022d72d79a1e508', {lat:'@lat', lon:'@lon'});
     	};
 
 	}])
@@ -29,36 +30,38 @@ angular.module('weatherApp', ['ngResource'])
 		$scope.detailedWeatherData = [];
 		$scope.city = '';
 		$scope.isLoading = false;
+		$scope.isError = false;
 
 		$scope.chooseCity = function() {
-		
+			$scope.isError = false;
 			$scope.isLoading = true;
 			$scope.currentWeatherData = false;
 			fetchWeatherData.getWeatherByCity().get({city:$scope.city})
 	    	.$promise.then(function(currentData) {
 	    		$scope.currentData = currentData;
-	      		console.log(currentData);
-	      		$scope.isLoading = false;
-	      		$scope.currentWeatherData = true;
+	      		if ($scope.currentData.cod >= "400"){
+	      			$scope.isLoading = false;
+	      			$scope.isError = true;
+				} else {					
+		      		$scope.isLoading = false;
+		      		$scope.isError = false;
+		      		$scope.currentWeatherData = true;
+		      		$scope.isCity = false;
+		      		getData($scope.city);
+				};
 	    	});
-
-	    	getData($scope.city);
-
-	    	
-	    	$scope.city = ''
+	    						
+	    	$scope.city = '';
 		};
 
 		navigator.geolocation.getCurrentPosition(function(location) {
 			$scope.lat= location.coords.latitude
 			$scope.lon = location.coords.longitude
-			console.log($scope.lat);
-			console.log($scope.lon);
 			$scope.isLoading = true;
 
 			fetchWeatherData.getWeatherByCoord().get({lat:$scope.lat, lon:$scope.lon})
 	    		.$promise.then(function(currentData) {
 	    			$scope.currentData = currentData;
-	      			console.log(currentData);
 	      			getData($scope.currentData.name);
 	      			$scope.currentWeatherData = true;
 	      			$scope.isLoading = false;
@@ -69,8 +72,6 @@ angular.module('weatherApp', ['ngResource'])
 			fetchWeatherData.getSummaryWeatherByCity().get({city:city})
 	    	.$promise.then(function(dailyData) {
 	    		$scope.dailyData = dailyData;
-	      		console.log(dailyData);
-	      		console.log(dailyData.list[0].dt);
 	      		$scope.changeDay(dailyData.city.name, dailyData.list[0].dt);
 	      		$scope.selectedDay = dailyData.list[0].dt;
 	    	});
@@ -78,7 +79,6 @@ angular.module('weatherApp', ['ngResource'])
 	    	fetchWeatherData.getDetailedWeatherByCity().get({city:city})
 	    	.$promise.then(function(hourlyData) {
 	    		$scope.hourlyData = hourlyData;
-	      		console.log(hourlyData);
 	      		$scope.detailedWeatherData = hourlyData.list;
 	    	});
 
@@ -89,7 +89,6 @@ angular.module('weatherApp', ['ngResource'])
 	    	fetchWeatherData.getDetailedWeatherByCity().get({city:city})
 	    	.$promise.then(function(hours) {
 	    		$scope.hours = hours;
-	      		console.log(hours);
 	    	});
 	    };
 
