f611017e7857adcc51d44e4dbad024415ebcef58
Unknown <claudia.zanetel@gmail.com>
2017-06-08 15:16:14 +0200

    added google analytics

--- js/config.template.js
+++ js/config.template.js
@@ -1,2 +1,8 @@
 angular.module('weatherApp')
 	.constant('OPENWEATHERMAP_APIKEY', '__OPENWEATHERMAP_APIKEY__');
+
+angular.module('weatherApp')
+	.config(['AnalyticsProvider', function (AnalyticsProvider) {
+   		AnalyticsProvider.setAccount('__GOOGLEANALYTICS_APIKEY__');
+	}])
+	.run(['Analytics', function(Analytics) { }]);
