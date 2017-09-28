d53555af58805e2691faca798037a34c663a3616
Claudia Zanetel <clza@Daresay3-MBP13-1709.local>
2017-09-28 16:56:40 +0200

    Add gulp watch and browser sync and compile sass to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
 
 //Default task
 gulp.task('default', ['bower']); 
-
 
 /*
 * downloads front-end dependencies
@@ -11,3 +13,37 @@ gulp.task('default', ['bower']);
 gulp.task('bower', function() {
     return bower('./components')
 });
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    })
+})
+
+gulp.task('sass', function() {
+    return gulp.src('styles/*.scss') // Gets all files ending with .scss in app/scss
+      .pipe(sass())
+      .pipe(gulp.dest('styles'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+    gulp.watch('styles/*.scss', ['sass']); 
+    gulp.watch('*.html', browserSync.reload); 
+    gulp.watch('js/*.js', browserSync.reload); 
+    // Other watchers
+})
+
+gulp.task('useref', function(){
+    return gulp.src('*.html')
+      .pipe(useref())
+      .pipe(gulp.dest('dist'))
+  });
+
+gulp.watch('styles*.scss', ['sass']); 
+
+
